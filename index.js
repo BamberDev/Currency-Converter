b8ebd9e3aa8733233eb4cb070c3eef55a615c508
Bamberek <kabaczek99@gmail.com>
2024-05-31 00:03:25 +0200

    updated js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const currencySelect = document.querySelector("#currency");
+const fromCurrencySelect = document.querySelector("#from-currency");
+const toCurrencySelect = document.querySelector("#to-currency");
 const amountInput = document.querySelector("#amount");
 const resultParagraph = document.querySelector("#result");
 const currencyForm = document.querySelector("#currency-form");
@@ -8,7 +9,8 @@ const loader = document.querySelector("#loader");
 
 async function convertCurrency() {
   loader.style.display = "block";
-  const currencyCode = currencySelect.value;
+  const fromCurrencyCode = fromCurrencySelect.value;
+  const toCurrencyCode = toCurrencySelect.value;
   const amount = amountInput.value;
 
   if (!amount || isNaN(amount) || amount <= 0) {
@@ -19,20 +21,25 @@ async function convertCurrency() {
   }
 
   try {
-    const response = await fetch(
-      `https://api.nbp.pl/api/exchangerates/rates/A/${currencyCode}/?format=json`
+    const fromResponse = await fetch(
+      `https://api.nbp.pl/api/exchangerates/rates/A/${fromCurrencyCode}/?format=json`
     );
-    const data = await response.json();
+    const fromData = await fromResponse.json();
+    const fromExchangeRate = fromData?.rates?.[0]?.mid;
 
-    const exchangeRate = data?.rates?.[0]?.mid;
+    const toResponse = await fetch(
+      `https://api.nbp.pl/api/exchangerates/rates/A/${toCurrencyCode}/?format=json`
+    );
+    const toData = await toResponse.json();
+    const toExchangeRate = toData?.rates?.[0]?.mid;
 
-    if (!exchangeRate) {
+    if (!fromExchangeRate || !toExchangeRate) {
       throw new Error("Exchange rate not available.");
     }
 
-    const result = (amount * exchangeRate).toFixed(2);
+    const result = ((amount * fromExchangeRate) / toExchangeRate).toFixed(2);
 
-    resultParagraph.textContent = `${amount} ${currencyCode} = ${result} PLN`;
+    resultParagraph.textContent = `${amount} ${fromCurrencyCode} = ${result} ${toCurrencyCode}`;
     resultParagraph.classList.remove("text-danger");
   } catch (error) {
     resultParagraph.textContent =
@@ -57,7 +64,15 @@ currencyForm.addEventListener("submit", function (event) {
   convertCurrency();
 });
 
-currencySelect.addEventListener("change", function () {
+fromCurrencySelect.addEventListener("change", function () {
+  if (amountInput.value !== "") {
+    convertCurrency();
+  } else {
+    resultParagraph.textContent = "";
+  }
+});
+
+toCurrencySelect.addEventListener("change", function () {
   if (amountInput.value !== "") {
     convertCurrency();
   } else {
